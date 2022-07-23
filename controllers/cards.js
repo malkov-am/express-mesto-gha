9ed2a14c61a1345e5311747a0a72f519cf5aabf5
Andrei Malkov <thewildcat@yandex.ru>
2022-07-23 15:46:43 +0300

    Add errors handling

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,31 +1,111 @@
 // Импорт модели
 const Card = require('../models/card');
 // Получение всех карточек
-function getCards(req, res) {
-  Card.find({})
-    .populate(['owner', 'likes'])
-    .then((cards) => res.send(cards));
+async function getCards(req, res) {
+  try {
+    const cards = await Card.find({}).populate(['owner', 'likes']);
+    res.send(cards);
+  } catch (err) {
+    res.status(500).send({
+      message: 'Внутренняя ошибка сервера.',
+    });
+  }
 }
 // Создание карточки
-function createCard(req, res) {
-  const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user }).then((card) => res.send(card));
+async function createCard(req, res) {
+  try {
+    const { name, link } = req.body;
+    const card = await Card.create({ name, link, owner: req.user });
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании карточки.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 // Удаление карточки
-function deleteCard(req, res) {
-  Card.findByIdAndRemove(req.params.cardId).then(() => res.send({ message: 'Пост удалён' }));
+async function deleteCard(req, res) {
+  try {
+    const cardToDelete = await Card.findByIdAndRemove(req.params.cardId);
+    if (!cardToDelete) {
+      res.status(404).send({
+        message: 'Карточка с указанным _id не найдена.',
+      });
+    } else {
+      res.send({ message: 'Пост удалён' });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: 'Внутренняя ошибка сервера.',
+    });
+  }
 }
 // Добавление лайка
-function addLike(req, res) {
-  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .populate(['owner', 'likes'])
-    .then((card) => res.send(card));
+async function addLike(req, res) {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    ).populate(['owner', 'likes']);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные для постановки/снятии лайка.',
+        });
+        break;
+      case 'CastError':
+        res.status(404).send({
+          message: 'Передан несуществующий _id карточки.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 // Снятие лайка
-function removeLike(req, res) {
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .populate(['owner', 'likes'])
-    .then((card) => res.send(card));
+async function removeLike(req, res) {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    ).populate(['owner', 'likes']);
+    res.send(card);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные для постановки/снятии лайка.',
+        });
+        break;
+      case 'CastError':
+        res.status(404).send({
+          message: 'Передан несуществующий _id карточки.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,33 +1,114 @@
 // Импорт модели
 const User = require('../models/user');
 // Получение всех пользователей
-function getUsers(req, res) {
-  User.find({}).then((usersData) => {
+async function getUsers(req, res) {
+  try {
+    const usersData = await User.find({});
     res.send(usersData);
-  });
+  } catch (err) {
+    res.status(500).send({
+      message: 'Внутренняя ошибка сервера.',
+    });
+  }
 }
+
 // Получение пользователя по id
-function getUser(req, res) {
-  User.findById(req.user._id).then((userData) => res.send(userData));
+async function getUser(req, res) {
+  try {
+    const userData = await User.findById(req.params.userId);
+    if (!userData) {
+      res.status(404).send({
+        message: 'Пользователь по указанному _id не найден.',
+      });
+      return;
+    }
+    res.send(userData);
+  } catch (err) {
+    res.status(500).send({
+      message: 'Внутренняя ошибка сервера.',
+    });
+  }
 }
 // Создание нового пользователя
-function createUser(req, res) {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar }).then((userData) => res.send(userData));
+async function createUser(req, res) {
+  try {
+    const { name, about, avatar } = req.body;
+    const userData = await User.create({ name, about, avatar });
+    res.send(userData);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 // Обновление профиля
-function updateProfile(req, res) {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true }).then(
-    (updatedUserData) => res.send(updatedUserData)
-  );
+async function updateProfile(req, res) {
+  try {
+    const { name, about } = req.body;
+    const updatedUserData = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true, runValidators: true },
+    );
+    res.send(updatedUserData);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля.',
+        });
+        break;
+      case 'CastError':
+        res.status(404).send({
+          message: 'Пользователь с указанным _id не найден.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 // Обновление аватара
-function updateAvatar(req, res) {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true }).then(
-    (updatedUserData) => res.send(updatedUserData)
-  );
+async function updateAvatar(req, res) {
+  try {
+    const { avatar } = req.body;
+    const updatedUserData = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true, runValidators: true },
+    );
+    res.send(updatedUserData);
+  } catch (err) {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении аватара.',
+        });
+        break;
+      case 'CastError':
+        res.status(404).send({
+          message: 'Пользователь с указанным _id не найден.',
+        });
+        break;
+
+      default:
+        res.status(500).send({
+          message: 'Внутренняя ошибка сервера.',
+        });
+    }
+  }
 }
 
 module.exports = {
